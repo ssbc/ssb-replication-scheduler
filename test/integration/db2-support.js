d04a378afdd514f76bf87483747d9a34e7e34f3a
Andre Staltz <andre@staltz.com>
2021-06-16 14:19:14 +0300

    try to support ssb-db2

---
+++ test/integration/db2-support.js
@@ -0,0 +1,105 @@
+const tape = require('tape')
+const crypto = require('crypto')
+const ssbKeys = require('ssb-keys')
+const path = require('path')
+const os = require('os')
+const rimraf = require('rimraf')
+const SecretStack = require('secret-stack')
+const pify = require('promisify-4loc')
+const sleep = require('util').promisify(setTimeout)
+
+const createSsbServer = SecretStack({
+  caps: { shs: crypto.randomBytes(32).toString('base64') }
+})
+  .use(require('ssb-db2'))
+  .use(require('ssb-db2/compat/ebt'))
+  .use(require('ssb-ebt'))
+  .use(require('ssb-friends'))
+  .use(require('../..'))
+
+const CONNECTION_TIMEOUT = 500 // ms
+const REPLICATION_TIMEOUT = 2 * CONNECTION_TIMEOUT
+
+tape('replicate between 3 peers, using ssb-db2', async (t) => {
+  t.plan(3)
+
+  rimraf.sync(path.join(os.tmpdir(), 'server-alice'))
+  rimraf.sync(path.join(os.tmpdir(), 'server-bob'))
+  rimraf.sync(path.join(os.tmpdir(), 'server-carol'))
+
+  const alice = createSsbServer({
+    path: path.join(os.tmpdir(), 'server-alice'),
+    keys: ssbKeys.generate(),
+    timeout: CONNECTION_TIMEOUT,
+  })
+  const bob = createSsbServer({
+    path: path.join(os.tmpdir(), 'server-bob'),
+    keys: ssbKeys.generate(),
+    timeout: CONNECTION_TIMEOUT,
+  })
+  const carol = createSsbServer({
+    path: path.join(os.tmpdir(), 'server-carol'),
+    keys: ssbKeys.generate(),
+    timeout: CONNECTION_TIMEOUT,
+  })
+
+  // Wait for all bots to be ready
+  await sleep(500)
+
+  await Promise.all([
+    // All peers publish a post
+    pify(alice.db.publish)({ type: 'post', text: 'hello' }),
+    pify(bob.db.publish)({ type: 'post', text: 'hello' }),
+    pify(carol.db.publish)({ type: 'post', text: 'hello' }),
+
+    // alice follows bob & carol
+    pify(alice.db.publish)({ type: 'contact', contact: bob.id, following: true }),
+    pify(alice.db.publish)({ type: 'contact', contact: carol.id, following: true }),
+
+    // bob follows alice & carol
+    pify(bob.db.publish)({ type: 'contact', contact: alice.id, following: true }),
+    pify(bob.db.publish)({ type: 'contact', contact: carol.id, following: true }),
+
+    // carol follows alice & bob
+    pify(carol.db.publish)({ type: 'contact', contact: alice.id, following: true }),
+    pify(carol.db.publish)({ type: 'contact', contact: bob.id, following: true }),
+  ])
+
+  const [connectionBA, connectionBC, connectionCA] = await Promise.all([
+    pify(bob.connect)(alice.getAddress()),
+    pify(bob.connect)(carol.getAddress()),
+    pify(carol.connect)(alice.getAddress()),
+  ])
+
+  const expectedClock = {
+    [alice.id]: 3,
+    [bob.id]: 3,
+    [carol.id]: 3,
+  }
+
+  await sleep(REPLICATION_TIMEOUT)
+
+  const [clockAlice, clockBob, clockCarol] = await Promise.all([
+    pify(alice.getVectorClock)(),
+    pify(bob.getVectorClock)(),
+    pify(carol.getVectorClock)(),
+  ])
+
+  t.deepEqual(clockAlice, expectedClock, 'alice\'s clock is correct')
+  t.deepEqual(clockBob, expectedClock, 'bob\'s clock is correct')
+  t.deepEqual(clockCarol, expectedClock, 'carol\'s clock is correct')
+
+  await Promise.all([
+    pify(connectionBA.close)(true),
+    pify(connectionBC.close)(true),
+    pify(connectionCA.close)(true),
+  ])
+
+  await Promise.all([
+    pify(alice.close)(true),
+    pify(bob.close)(true),
+    pify(carol.close)(true)
+  ])
+
+  t.end()
+})
