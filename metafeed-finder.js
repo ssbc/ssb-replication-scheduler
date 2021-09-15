c7ec4c60eb334bc657dfb90ef3ad24791573df12
Andre Staltz <andre@staltz.com>
2021-09-15 15:08:33 +0300

    MetafeedFinder uses metafeeds.find in case of self ID

--- metafeed-finder.js
+++ metafeed-finder.js
@@ -26,6 +26,18 @@ module.exports = class MetafeedFinder {
       const metaFeedId = this._map.get(mainFeedId)
       cb(null, metaFeedId)
       return
+    } else if (mainFeedId === this._ssb.id) {
+      this._ssb.metafeeds.find((err, rootMF) => {
+        if (err) {
+          return cb(err)
+        } else if (!rootMF) {
+          cb(null, null)
+        } else {
+          const metaFeedId = rootMF.keys.id
+          this._map.set(mainFeedId, metaFeedId)
+          cb(null, metaFeedId)
+        }
+      })
     } else {
       this._request(mainFeedId, cb)
     }
