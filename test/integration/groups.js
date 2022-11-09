854210f1c26a423274173d5496b2ce1541c869fe
Jacob Karlsson <jacob.karlsson95@gmail.com>
2022-11-09 13:31:07 +0100

    Start installing group stuff

---
+++ test/integration/groups.js
@@ -0,0 +1,63 @@
+const test = require('tape')
+const rimraf = require('rimraf')
+const SecretStack = require('secret-stack')
+const caps = require('ssb-caps')
+const p = require('promisify-4loc')
+const u = require('../misc/util')
+
+const Server = (name, opts = {}) => {
+  const path = `/tmp/ssb-replication-tests-${name}`
+  rimraf.sync(path)
+
+  return SecretStack({ caps })
+    .use(require('ssb-db2'))
+    .use(require('ssb-db2/compat/ebt'))
+    .use(require('ssb-bendy-butt'))
+    .use(require('ssb-conn'))
+    .use(require('ssb-ebt'))
+    .use(require('ssb-friends'))
+    .use(require('ssb-meta-feeds'))
+    .use(require('ssb-subset-rpc'))
+    .use(require('ssb-index-feeds'))
+    .use(require('ssb-tribes2'))
+    .use(require('../..'))({
+    path,
+    ...opts,
+  })
+}
+
+test('You replicate other people in a group', async (t) => {
+  const alice = Server('alice', {
+    friends: {
+      hops: 1,
+    },
+  })
+
+  const bob = Server('bob', {
+    friends: {
+      hops: 1,
+    },
+    replicationScheduler: {
+      debouncePeriod: 1,
+      partialReplication: {
+        1: [{ purpose: 'main' }],
+        group: [{ purpose: '$groupSecret' }],
+      },
+    },
+  })
+
+  const carol = Server('carol', {
+    friends: {
+      hops: 1,
+    },
+  })
+
+  await Promise.all([
+    p(alice.db.publish)(u.follow(bob.id)),
+    p(alice.db.publish)(u.follow(carol.id)),
+    p(bob.db.publish)(u.follow(alice.id)),
+    p(carol.db.publish)(u.follow(alice.id)),
+  ])
+
+  const group = await alice.tribes2.create()
+})
